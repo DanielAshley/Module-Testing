ef904bd8348ca75d571e344eefbc53e725fb2e9d
dashley101 <dashley101@gmail.com>
2014-11-12 14:30:15 -0500

    testing wmra before omni tests

--- Test Program/Source/testInputModules/testInputModules/main.cpp
+++ Test Program/Source/testInputModules/testInputModules/main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 
-int main()
+int moveSquare()
 {
 	WMRA::Pose curPos;
 	WMRA::WMRA_module wmraArm;
@@ -43,7 +43,7 @@ int main()
 		
 
 	// Move arm to starting position (dest1)
-	wmraArm.autonomous(dest1, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+	wmraArm.autonomous(dest1, WMRA::ARM_FRAME_PILOT_MODE,true); // Moves arm
 	Sleep(5000); // wait 5 seconds before starting
 
 	while(loopCount > 0 && choice==1)
@@ -56,64 +56,46 @@ int main()
 		loopCount--;
 	}
 
-	wmraArm.autonomous(dest1, WMRA::ARM_FRAME_PILOT_MODE,true); // Moves arm start location
+	Sleep(2000); // wait 2 seconds
 
 	wmraArm.toReady(); // moves arm to ready position
 
-	return 0;
+	return 1;
 }
 
 
 
-//int main()
-//{
-//	float gain = 0.03;
-//	omni Om;
-//	WMRA::Pose p, delta, tgt;
-//	Arm a;
-//	a.initialize();
-//	Sleep(5000);
-//	double pitch, roll, yaw;
-//
-//	vector<double> tran(16);
-//
-//	while(1)
-//	{
-//		Sleep(1000);
-//		delta = Om.getDeltaPose();
-////		cout << "Omni Delta Pose: " << delta.x << ", " << delta.y << ", " << delta.z << ", " << delta.roll << ", " << delta.pitch << ", " << delta.yaw << endl;
-//
-//		
-//		tran = Om.getKinematicPose();
-//		cout << "\n\n\t| " << tran[0] << "\t" << tran[4] << "\t" << tran[8] << "\t" << tran[12] << "\t|";
-//		cout << "\n\t| " << tran[1] << "\t" << tran[5] << "\t" << tran[9] << "\t" << tran[13] << "\t|";
-//		cout << "\n\t| " << tran[2] << "\t" << tran[6] << "\t" << tran[10] << "\t" << tran[14] << "\t|";
-//		cout << "\n\t| " << tran[3] << "\t\t" << tran[7] << "\t\t" << tran[11] << "\t\t" << tran[15] << "\t\t|\n\n";
-//
-//		pitch = atan2((-1*tran[2]), sqrt((tran[0]*tran[0])+(tran[1]*tran[1])));
-//		roll = atan2((tran[1]/cos(pitch)), (tran[0]/cos(pitch)));
-//		yaw = atan2((tran[6]/cos(pitch)), (tran[10]/cos(pitch)));
-//
-//		cout << "Pitch: " << pitch << "\t\tRoll: " << roll << "\t\tYaw: " << yaw << endl;
-//
-//		p = a.getPose();
-//		//cout << "Arm Pose: " << p.x << ", " << p.y << ", " << p.z << ", " << p.roll << ", " << p.pitch << ", " << p.yaw << endl;
-//
-//		if(delta.x != 0 || delta.y != 0 || delta.z != 0)
-//		{
-//			tgt.x = p.x + (delta.x*gain);
-//			tgt.y = p.y + (delta.y*gain);
-//			tgt.z = p.z + (delta.z*gain);
-//
-//			tgt.pitch = p.pitch;// + delta.pitch;
-//			tgt.roll = p.roll;// + delta.roll;
-//			tgt.yaw = p.yaw;// + delta.yaw;
-////			cout << "Arm Pose: " << p.x << ", " << p.y << ", " << p.z << ", " << p.roll << ", " << p.pitch << ", " << p.yaw << endl;
-////			cout << "Sent Pose: " << tgt.x << ", " << tgt.y << ", " << tgt.z << ", " << tgt.roll << ", " << tgt.pitch << ", " << tgt.yaw << endl;
-//
-//			a.teleoperation(tgt, WMRA::ARM_FRAME_PILOT_MODE);
-//		}
-//		
-//	}
-//	return 0;
-//}
+int main()
+{
+	WMRA::Pose curPos;
+	WMRA::WMRA_module wmraArm;
+	wmraArm.initialize();
+	Sleep(5000);
+
+	curPos = wmraArm.getPose();
+
+	for(int i = 0; i<4; i++)
+	{
+		curPos.x = curPos.x+10;
+		wmraArm.autonomous(curPos, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+	}
+	for(int i = 0; i<4; i++)
+	{
+		curPos.y = curPos.y+10;
+		wmraArm.autonomous(curPos, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+	}
+	for(int i = 0; i<4; i++)
+	{
+		curPos.x = curPos.x-10;
+		wmraArm.autonomous(curPos, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+	}
+	for(int i = 0; i<4; i++)
+	{
+		curPos.y = curPos.y-10;
+		wmraArm.autonomous(curPos, WMRA::ARM_FRAME_PILOT_MODE); // Moves arm
+	}	
+	Sleep(12000); // wait 2 seconds
+	
+	wmraArm.toReady(); // moves arm to ready position
+	return 0;
+}
